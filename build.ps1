9b3df49983ab18dcbf92eb92c29e62e4f9a979d6
Louis-Marie Matthews <matthewslouismarie@gmail.com>
2023-05-01 01:41:54 +0200

    Make compilation optional

--- build.ps1
+++ build.ps1
@@ -8,6 +8,7 @@
 # compile smd
 # GUI
 # DONE Remove dev entities
+# @todo Validate config file, check that code should throw error if config file is invalid or field is not specified
 
 # Paths specified in the configuration file must not have trailing slashes.
 
@@ -35,73 +36,86 @@ New-Item -Name $build -ItemType Directory -Force
 $pwd = Get-Location
 Set-Location $build
 try {
-    if (Test-Path -Path "$($config.hlradFileFld)/$mapname.rad") {
-        Copy-Item "$($config.hlradFileFld)/$mapname.rad" -Destination .
+    if ($config.compile -and (Test-Path -Path "$($config.compileRadFileFld)/$mapname.rad")) {
+        Copy-Item "$($config.compileRadFileFld)/$mapname.rad" -Destination .
     }
 
-    if ($config.wadToBuild -ne $null) {
-        Invoke-Expression "$($config.wadmaker) -subdirs -nologfile $($config.wadTexturesFld) $($config.assetFld)/$($config.wadToBuild)"
+    if ($config._wadMaker -ne $null) {
+        Invoke-Expression "$($config._wadMaker) -subdirs -nologfile $($config._wadMakerTexturesFld) $($config.assetFld)/$($config._wadMakerWadName)"
     }
 
     foreach ($wad in $config.wads) {
         Copy-Item "$($config.assetFld)/$wad" -Destination .
     }
 
-    if ($config.assetSprToBuildFld -ne $null) {
-        Invoke-Expression "$($config.spritemaker) -subdirs -nologfile $($config.assetSprToBuildFld) $($config.assetFld)/sprites"
+    if ($config._sprMaker -ne $null) {
+        Invoke-Expression "$($config._sprMaker) -subdirs -nologfile $($config._sprMakerFldToBuild) $($config.assetFld)/sprites"
     }
 
-    # if ($config.studiomdl -ne $null -and $config.mdlToBuild -ne $null) {
+    # if ($config._studiomdl -ne $null -and $config.mdlToBuild -ne $null) {
     #     foreach ($smd in Get-ChildItem -Include '*.qc' -Path $config.mdlToBuild -Recurse) {
-    #         Invoke-Expression "$($config.studiomdl) $($config.mdlToBuild)/$($smd.Name)"
+    #         Invoke-Expression "$($config._studiomdl) $($config.mdlToBuild)/$($smd.Name)"
     #     }
     #     foreach ($mdl in Get-ChildItem -Include '*.mdl' -Path $config.mdlToBuild) {
     #         Copy-Item $mdl -Destination $config.mdlFld
     #     }
     # }
 
-    if ($config.mapExporter -ceq "mess") {
-        Copy-Item "$($config.rmfFld)/$mapname.rmf" -Destination .
-        Invoke-Expression "$($config.mess) -dir $($config.messTemplatesFld) -log verbose $mapname.rmf $mapname.map"
-        (Get-Content "$mapname.map") | Foreach-Object {
-            $_
-            if ($_ -like '"classname" "worldspawn"') {
-                '"wad" "' + ($config.wads -join ";") + '"'
-            }
-        } | Set-Content "$mapname.map"
-    } elseif ($config.mapExporter -ceq $null) {
-        Copy-Item "$($config.rmfFld)/$mapname.map" -Destination .
+    if ($config.map -eq $true) {
+        if ($config.mapExporter -ceq "mess") {
+            Copy-Item "$($config.mapRmfFld)/$mapname.rmf" -Destination .
+            Invoke-Expression "$($config._mess) -dir $($config._messTemplatesFld) -log verbose $mapname.rmf $mapname.map"
+            (Get-Content "$mapname.map") | Foreach-Object {
+                $_
+                if ($_ -like '"classname" "worldspawn"') {
+                    '"wad" "' + ($config.wads -join ";") + '"'
+                }
+            } | Set-Content "$mapname.map"
+        } elseif ($config.mapExporter -eq $null) {
+            Copy-Item "$($config.mapRmfFld)/$mapname.map" -Destination .
+        } else {
+            throw "Invalid mapExporter value."
+        }
+
+        if ($config._mess -ne $null) {
+            Invoke-Expression "$($config._mess) -dir $($config._messTemplatesFld) $($config._messParams) $mapname"
+        }
+
+        if ($config.prod -eq $true) {
+            (Get-Content "$mapname.map") -Replace '"targetname" "mm_devmapstart"', '"targetname" "mm_devmapstart_dis"' | Set-Content "$mapname.map"
+        }
     }
 
-    if ($prod) {
-        (Get-Content "$mapname.map") -Replace '"targetname" "mm_devmapstart"', '"targetname" "mm_devmapstart_dis"' | Set-Content "$mapname.map"
-    }
-
-    Invoke-Expression "$($config.mess) -dir $($config.messTemplatesFld) $($config.messParams) $mapname"
-    Invoke-Expression "$($config.hlcsg) $($config.hlcsgParams) $mapname"
-    if (!($config.hlcsgParams -Match "-onlyents")) {
-        Invoke-Expression "$($config.hlbsp) $($config.hlbspParams) $mapname"
-        Invoke-Expression "$($config.hlvis) $($config.hlvisParams) $mapname"
-        Invoke-Expression "$($config.hlrad) $($config.hlradParams) $mapname"
+    if ($config.compile) {
+        Invoke-Expression "$($config.compileCsg) $($config.compileCsgParams) $mapname"
+        if (!($config.compileCsgParams -Match "-onlyents")) {
+            Invoke-Expression "$($config.compileBsp) $($config.compileBspParams) $mapname"
+            Invoke-Expression "$($config.compileVis) $($config.compileVisParams) $mapname"
+            Invoke-Expression "$($config.compileRad) $($config.compileRadParams) $mapname"
+        }
+    } else {
+        Copy-Item "$($config.assetFld)/maps/$mapname.bsp" -Destination .
     }
 
     New-Item -Name "release" -ItemType Directory -Force
     Set-Location "release"
     New-Item -Name "maps" -ItemType Directory -Force
     Copy-Item "../$mapname.bsp" -Destination "maps"
-    Copy-Item $config.resguyIgnore -Destination .
-    Invoke-Expression "$($config.resguy) $mapname -missing"
 
-    foreach ($l in Get-Content -Path "maps/$mapname.res") {
-        if ($l -match '^\w+\.wad$') {
-            Copy-Item "../$l" -Destination .
-        } elseif ($l -match '^\w+') {
-            New-Item -Path ([regex]::match($l, '^[\w/]+/').Groups[0].Value) -ItemType Directory -Force
-            Copy-Item "$($config.assetFld)/$l" -Destination $l -Force
+    if ($config._resguy -ne $null) {
+        Copy-Item $config._resguyIgnore -Destination .
+        Invoke-Expression "$($config._resguy) $mapname -missing" > "../$mapname.res.log"
+
+        foreach ($l in Get-Content -Path "maps/$mapname.res") {
+            if ($l -match '^\w+\.wad$') {
+                Copy-Item "../$l" -Destination .
+            } elseif ($l -match '^\w+') {
+                New-Item -Path ([regex]::match($l, '^[\w/]+/').Groups[0].Value) -ItemType Directory -Force
+                Copy-Item "$($config.assetFld)/$l" -Destination $l -Force
+            }
         }
+        Remove-Item ([regex]::match($config._resguyIgnore, '\w+\.txt$').Groups[0].Value) -Force
     }
-
-    Remove-Item ([regex]::match($config.resguyIgnore, '\w+\.txt$').Groups[0].Value) -Force
 
     if (Test-Path -Path "$($config.assetFld)/maps/$mapname.cfg") {
         Copy-Item "$($config.assetFld)/maps/$mapname.cfg" -Destination "maps" -Force
@@ -111,8 +125,8 @@ try {
         Copy-Item "$($config.assetFld)/scripts" -Destination . -Force -Recurse
     }
 
-    if ($config.releaseCopyFld -ne $null) {
-        Copy-Item -Path "*" -Destination $config.releaseCopyFld -Force -Recurse
+    if ($config._releaseCopyFld -ne $null) {
+        Copy-Item -Path "*" -Destination $config._releaseCopyFld -Force -Recurse
     }
 } finally {
     Set-Location $pwd
